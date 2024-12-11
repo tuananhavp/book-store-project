9d81d1c972012248bcb80094f0c98291063d81df
tuan anh <tuananhavp@gmail.com>
2024-12-11 14:12:36 +0700

    add responsive checkout page and useform

--- frontend/src/pages/books/CheckoutPage.jsx
+++ frontend/src/pages/books/CheckoutPage.jsx
@@ -1,4 +1,6 @@
 import { useSelector } from "react-redux";
+import { useForm } from "react-hook-form";
+import { use, useState } from "react";
 
 const CheckOutPage = () => {
   const cartItems = useSelector((state) => state.cart.cartItems);
@@ -7,11 +9,36 @@ const CheckOutPage = () => {
     .reduce((total, item) => total + item.newPrice, 0)
     .toFixed(2);
 
+  const [isChecked, setIsChecked] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    const newOrder = {
+      name: data.name,
+      email: data.email,
+      address: {
+        city: data.city,
+        country: data.country,
+        state: data.state,
+        zipcode: data.zipcode,
+      },
+      phone: data.phone,
+      productIds: cartItems.map((item) => item?._id),
+      totalPrice: totalPrice,
+    };
+    console.log(newOrder);
+  };
+
   return (
-    <section className="w-full bg-gray-100 min-h-dvh">
-      <div className="max-w-6xl mx-auto pt-40">
+    <section className="w-full sm:bg-gray-100 bg-white min-h-dvh">
+      <div className="max-w-6xl mx-auto pt-20 sm:px-10 py-20">
         {/* Top side */}
-        <div>
+        <div className="px-5">
           <h3 className="font-semibold text-xl text-gray-600 mb-3">
             Cash On Delevary
           </h3>
@@ -19,22 +46,23 @@ const CheckOutPage = () => {
           <p className="text-gray-400 mb-3">Items: {cartItems.length}</p>
         </div>
         {/* Bottom side */}
-        <div className="min-h-screen bg-white">
-          <div className="px-10 pt-20 flex justify-between">
+        <div className=" pb-20 bg-white">
+          <div className="sm:px-10 px-5 pt-20 sm:flex justify-between">
             {/* Left Side */}
-            <div>
+            <div className="pr-3">
               <h3 className="font-semibold text-lg text-gray-500 mb-3">
                 Personal Details
               </h3>
               <p className="text-gray-500">Please fill out all the fields</p>
             </div>
             {/* Right Side */}
-            <div className="w-2/3">
-              <form action="">
+            <div className="sm:w-2/3 w-full">
+              <form onSubmit={handleSubmit(onSubmit)}>
                 {/* Name */}
                 <div className="mb-5">
                   <label htmlFor="name">Full Name</label>
                   <input
+                    {...register("name", { required: true })}
                     type="text"
                     id="name"
                     name="name"
@@ -45,6 +73,7 @@ const CheckOutPage = () => {
                 <div className="mb-5">
                   <label htmlFor="email">Email Address</label>
                   <input
+                    {...register("email", { required: true })}
                     type="email"
                     id="email"
                     name="email"
@@ -55,6 +84,7 @@ const CheckOutPage = () => {
                 <div className="mb-5">
                   <label htmlFor="phone">Phone Number</label>
                   <input
+                    {...register("phone", { required: true })}
                     type="tel"
                     id="phone"
                     name="phone"
@@ -62,10 +92,11 @@ const CheckOutPage = () => {
                   />
                 </div>
                 {/* Address/Street and City */}
-                <div className="flex gap-4 justify-between mb-6">
-                  <div className="w-2/3">
+                <div className="lg:flex gap-4 justify-between mb-6">
+                  <div className="xl:w-2/3 w-full">
                     <label htmlFor="address">Address / Street</label>
                     <input
+                      {...register("address", { required: true })}
                       type="text"
                       id="address"
                       name="address"
@@ -75,7 +106,8 @@ const CheckOutPage = () => {
                   <div>
                     <label htmlFor="city">City</label>
                     <input
-                      type="tel"
+                      {...register("city", { required: true })}
+                      type="text"
                       id="city"
                       name="city"
                       className="w-full border-2 p-2 mt-3 bg-gray-100 focus:outline-none"
@@ -83,20 +115,22 @@ const CheckOutPage = () => {
                   </div>
                 </div>
                 {/* Country / State / Zipcode*/}
-                <div className="flex gap-4 justify-between">
-                  <div className="w-3/4">
+                <div className="lg:flex gap-4 justify-between ">
+                  <div className="xl:w-3/4 w-full">
                     <label htmlFor="country">Country / region</label>
                     <input
+                      {...register("country", { required: true })}
                       type="text"
                       id="country"
                       name="country"
                       className="w-full border-2 p-2 mt-3 bg-gray-100 focus:outline-none"
                     />
                   </div>
-                  <div className="w-3/4">
+                  <div className="2xl:w-3/4 w-full">
                     <label htmlFor="state">State / province</label>
                     <input
-                      type="tel"
+                      {...register("state", { required: true })}
+                      type="text"
                       id="state"
                       name="state"
                       className="w-full border-2 p-2 mt-3 bg-gray-100 focus:outline-none"
@@ -105,7 +139,8 @@ const CheckOutPage = () => {
                   <div>
                     <label htmlFor="zipcode">Zipcode</label>
                     <input
-                      type="tel"
+                      {...register("zipcode", { required: true })}
+                      type="text"
                       id="zipcode"
                       name="zipcode"
                       className="w-full border-2 p-2 mt-3 bg-gray-100 focus:outline-none"
@@ -114,7 +149,12 @@ const CheckOutPage = () => {
                 </div>
                 {/* Checkbox */}
                 <div className="flex gap-3 mt-8">
-                  <input type="checkbox" />
+                  <input
+                    onChange={(e) => {
+                      setIsChecked(e.target.checked);
+                    }}
+                    type="checkbox"
+                  />
                   <p>
                     I am agree to the &nbsp;
                     <a href="" className="text-blue-400 underline">
@@ -127,7 +167,10 @@ const CheckOutPage = () => {
                   </p>
                 </div>
                 {/* Button */}
-                <button className=" mt-10 px-3 py-2 font-secondary font-bold rounded-lg bg-blue-500 hover:opcity-85 text-white ">
+                <button
+                  disabled={!isChecked}
+                  className=" mt-10 px-3 py-2 font-secondary font-bold rounded-lg bg-blue-500 hover:opcity-85 text-white "
+                >
                   Place an Order
                 </button>
               </form>
